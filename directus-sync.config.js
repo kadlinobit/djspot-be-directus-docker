22ba7eb108a99e855a5599bc6c87b96f53eee19d
Jan Kadlec <kadlec@cngroup.dk>
2026-04-15 15:37:44 +0200

    chore: remove deprecated directus-sync.config.js configuration file

--- directus-sync.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  directusUrl: 'http://localhost:8055',
-  directusEmail: 'admin@example.com',
-  directusPassword: 'pass',
-  dumpPath: './directus-config',
-  sortJson: true,
-  collections: true,
-}
